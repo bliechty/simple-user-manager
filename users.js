1b00b3e514aa1f141fddc34cb39b16ef9ce3afd5
Brady Liechty <berrybloxinator@gmail.com>
2020-01-24 08:59:22 -0700

    Switched index.html with index.pug. Deleted searchUsers and sessionUsers. Deleted error handling because it was breaking the program

--- users.js
+++ users.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 const bodyParser = require('body-parser');
 const fs = require("fs");
 const app = express();
+const path = require("path");
 const users = [];
 let port = process.env.PORT || 8080;
 
@@ -17,13 +18,18 @@ fs.createReadStream("./users.csv")
 const csvWriter = new createObjectCsvWriter({
     path: "users.csv",
     header: [
+        "username",
         "firstName",
         "lastName",
         "email",
-        "timeStamp"
+        "age",
+        "timeCreated"
     ],
     append: true
 });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -36,68 +42,48 @@ app.use(session({
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 10000 }
+    cookie: { maxAge: 100000 }
 }));
 
-app.use("/createUser", express.static("./createUser"));
-app.use("/searchUsers", express.static("./searchUsers"));
-app.use("/sessionUsers", (req, res, next) => {
-    if (!req.session.numSessionUsers) {
-        res.end("There are no users created in this session");
-    } else {
-        res.end(`There are ${req.session.numSessionUsers} users created in this session`);
-    }
+app.get("/createUser", (req, res) => {
+    res.render("index");
 });
 
-app.post("/createUser", (req, res, next) => {
+app.post("/createUser", (req, res) => {
     const d = new Date();
     const firstName = req.body["first-name"];
     const user = {
+        username: req.body["user-name"],
         firstName,
         lastName: req.body["last-name"],
         email: req.body["email-address"],
-        timeStamp: d.toLocaleString()
+        age: req.body["age"],
+        timeCreated: d.toLocaleString()
     };
     users.push(user);
     csvWriter.writeRecords([user]).then(() => {
         console.log("User written to csv file");
     });
-    if (!req.session.numSessionUsers) {
-        req.session.numSessionUsers = 1;
-    } else {
-        req.session.numSessionUsers++;
-    }
-    res.send(`<div>Thank you for making an account ${firstName}</div>`);
+    res.send(`<div>Thank you for making an account ${firstName}</div>
+              <a href="/createUser">Create User</a>`);
 });
 
 app.get("/", (req, res) => {
-    res.send("test");
-});
-
-app.post("/searchUsers", (req, res) => {
-    const user = users.filter(userObj => userObj.email === req.body["email-address"])[0];
-    if (user) {
-        res.send(`<div>First Name: ${user.firstName} LastName: ${user.lastName}</div>`);
-    } else {
-        res.send("user not found");
-    }
+    res.send("Home Page");
 });
 
 app.get("/allUsers", (req, res) => {
     for (let user of users) {
+        res.write(`Username: ${user.username}\n`);
         res.write(`First Name: ${user.firstName}\n`);
         res.write(`Last Name: ${user.lastName}\n`);
         res.write(`Email: ${user.email}\n`);
-        res.write(`Account created: ${user.timeStamp}\n\n\n`);
+        res.write(`Age: ${user.age}\n`);
+        res.write(`Account created: ${user.timeCreated}\n\n\n`);
     }
     res.end("End of user list");
 });
 
-app.use((err, req, res) => {
-    console.log("Error");
-    res.status(500).send("Something broke");
-});
-
 app.listen(port, () => {
-    console.log("listening on port 8080");
+    console.log("listening on port " + port);
 });
